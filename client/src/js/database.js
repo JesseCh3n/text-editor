36be0f004271e85bf5e2812bf17eed3b340b84ce
JesseCh3n <105155682+JesseCh3n@users.noreply.github.com>
2023-09-30 22:36:48 +1000

    Added idb and Webpack

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,52 @@ const initdb = async () =>
     },
   });
 
+export const postDb = async (content) => {
+  console.log('POST to the database'); 
+  
+  const jakeDb = await openDB('jake', 1);
+
+  const tx = jakeDb.transaction('jake', 'readwrite');
+
+  const store = tx.objectStore('jake');
+
+  const request = store.add({ jake: content });
+  
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('PUT to the database'); 
+  
+  const jakeDb = await openDB('jake', 1);
+
+  const tx = jakeDb.transaction('jake', 'readwrite');
+
+  const store = tx.objectStore('jake');
+
+  const request = store.put({ id: id, jake: content });
+
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+
+  const jaketDb = await openDB('jake', 1);
+
+  const tx = jaketDb.transaction('jake', 'readonly');
+
+  const store = tx.objectStore('jake');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
 
 initdb();
